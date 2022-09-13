045791ee4d47383b9e97d6dbfd6417f672c6f2b5
Maria del Mar Cuadros <mariadelmar.cuadros@zemoga.com>
2022-09-13 12:10:33 -0500

    Initial project config

---
+++ src/views/HomeView.vue
@@ -0,0 +1,9 @@
+<script setup>
+import HelloWorld from "../components/HelloWorld.vue";
+</script>
+
+<template>
+  <main class="bg-red-300 text-3xl font-bold underline">
+    <HelloWorld msg="Holiii" />
+  </main>
+</template>
